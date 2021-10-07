49cb4a78a0c25bc28ffd0d8997aeaad22e8f94f7
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-07 23:33:02 +0300

    ex done

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -17,10 +17,7 @@ package org.springframework.samples.petclinic.model;
 
 import java.io.Serializable;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for objects
