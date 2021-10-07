2ba1f4977c1d9cccd4633f679651cbad3f49d1ab
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-07 23:20:52 +0300

    Initial commit

---
+++ src/main/resources/db/h2/schema.sql
@@ -0,0 +1,64 @@
+-- DROP TABLE vet_specialties IF EXISTS;
+-- DROP TABLE vets IF EXISTS;
+-- DROP TABLE specialties IF EXISTS;
+-- DROP TABLE visits IF EXISTS;
+-- DROP TABLE pets IF EXISTS;
+-- DROP TABLE types IF EXISTS;
+-- DROP TABLE owners IF EXISTS;
+
+
+CREATE TABLE vets IF NOT EXISTS(
+  id         INTEGER IDENTITY PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name  VARCHAR(30)
+);
+CREATE INDEX vets_last_name ON vets (last_name);
+
+CREATE TABLE specialties IF NOT EXISTS(
+  id   INTEGER IDENTITY PRIMARY KEY,
+  name VARCHAR(80)
+);
+CREATE INDEX specialties_name ON specialties (name);
+
+CREATE TABLE vet_specialties IF NOT EXISTS(
+  vet_id       INTEGER NOT NULL,
+  specialty_id INTEGER NOT NULL
+);
+ALTER TABLE vet_specialties ADD CONSTRAINT fk_vet_specialties_vets FOREIGN KEY (vet_id) REFERENCES vets (id);
+ALTER TABLE vet_specialties ADD CONSTRAINT fk_vet_specialties_specialties FOREIGN KEY (specialty_id) REFERENCES specialties (id);
+
+CREATE TABLE types IF NOT EXISTS(
+  id   INTEGER IDENTITY PRIMARY KEY,
+  name VARCHAR(80)
+);
+CREATE INDEX types_name ON types (name);
+
+CREATE TABLE owners IF NOT EXISTS(
+  id         INTEGER IDENTITY PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name  VARCHAR_IGNORECASE(30),
+  address    VARCHAR(255),
+  city       VARCHAR(80),
+  telephone  VARCHAR(20)
+);
+CREATE INDEX owners_last_name ON owners (last_name);
+
+CREATE TABLE pets IF NOT EXISTS(
+  id         INTEGER IDENTITY PRIMARY KEY,
+  name       VARCHAR(30),
+  birth_date DATE,
+  type_id    INTEGER NOT NULL,
+  owner_id   INTEGER NOT NULL
+);
+ALTER TABLE pets ADD CONSTRAINT fk_pets_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
+ALTER TABLE pets ADD CONSTRAINT fk_pets_types FOREIGN KEY (type_id) REFERENCES types (id);
+CREATE INDEX pets_name ON pets (name);
+
+CREATE TABLE visits IF NOT EXISTS(
+  id          INTEGER IDENTITY PRIMARY KEY,
+  pet_id      INTEGER NOT NULL,
+  visit_date  DATE,
+  description VARCHAR(255)
+);
+ALTER TABLE visits ADD CONSTRAINT fk_visits_pets FOREIGN KEY (pet_id) REFERENCES pets (id);
+CREATE INDEX visits_pet_id ON visits (pet_id);
