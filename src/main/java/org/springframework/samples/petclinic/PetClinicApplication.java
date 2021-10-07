1ec79b6d8b8c17fab6b6715b6599996876211e81
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-08 00:10:29 +0300

    ex sqlite done

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -16,8 +16,14 @@
 
 package org.springframework.samples.petclinic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * PetClinic Spring Boot Application.
@@ -31,5 +37,15 @@ public class PetClinicApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
+	@Autowired Environment env;
 
+	@Bean
+	public DataSource dataSource() {
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("driverClassName"));
+		dataSource.setUrl(env.getProperty("url"));
+		dataSource.setUsername(env.getProperty("user"));
+		dataSource.setPassword(env.getProperty("password"));
+		return dataSource;
+	}
 }
